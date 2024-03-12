8ba19b3cb8fa1b565a19c4e559c3f767a0f4ea30
Omixxx <xxcortex99@gmail.com>
2024-03-12 09:23:16 +0100

    update

--- plugins.lua
+++ plugins.lua
@@ -365,7 +365,7 @@ local plugins = {
             enable = true,
             lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
             keymaps = {
-              ["va="] = { query = "@assignment.outer", desc = "Select outer part of an assignment" },
+              ["a="] = { query = "@assignment.outer", desc = "Select outer part of an assignment" },
               ["i="] = { query = "@assignment.inner", desc = "Select inner part of an assignment" },
               ["l="] = { query = "@assignment.lhs", desc = "Select left hand side of an assignment" },
               ["r="] = { query = "@assignment.rhs", desc = "Select right hand side of an assignment" },
